092eabb326b693928aa868fe2562ded2e29bd269
Plusb Preco <plusb21@gmail.com>
2018-04-25 12:14:52 +0900

    Fix `devtron` while disabling node integration same time

--- packages/electron-playgrounds/src/preload.js
+++ packages/electron-playgrounds/src/preload.js
@@ -6,3 +6,8 @@ window.ipc = ipc;
 // Disable `eval`
 // eslint-disable-next-line no-eval
 window.eval = null;
+
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line no-underscore-dangle
+  window.__devtron = {require, process};
+}
