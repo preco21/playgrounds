50fa80ed4fb6d3f4028286ec5560b4c2dbfc755e
Plusb Preco <plusb21@gmail.com>
2018-05-23 12:21:02 +0900

    We don't need `babel-runtime` everywhere

--- packages/electron-playgrounds/.babelrc.js
+++ packages/electron-playgrounds/.babelrc.js
@@ -35,9 +35,7 @@ module.exports = {
     ],
   plugins: [
     ...isMain
-      ? [
-        '@babel/plugin-transform-runtime',
-      ]
+      ? []
       : [
         '@babel/plugin-proposal-export-default-from',
         '@babel/plugin-proposal-export-namespace-from',
