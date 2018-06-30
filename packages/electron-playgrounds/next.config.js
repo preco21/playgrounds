45c9b3229bfc8a3e4125e8a083724d24a17204f8
Plusb Preco <plusb21@gmail.com>
2018-07-01 05:36:58 +0900

    Add support for `url-loader`

--- packages/electron-playgrounds/next.config.js
+++ packages/electron-playgrounds/next.config.js
@@ -3,6 +3,16 @@ const globby = require('globby');
 const {app: {rendererSource}} = require('./package.json');
 
 module.exports = withCSS({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+      },
+    });
+
+    return config;
+  },
   exportPathMap() {
     return globby([`${rendererSource}/pages/**/*.js`, `!${rendererSource}/pages/_document.js`])
       .then((paths) => paths.reduce((res, path) => {
