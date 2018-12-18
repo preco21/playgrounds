47784b144b3d7b79e1e1c3f966e5dfc5e9cc0e1b
Plusb Preco <plusb21@gmail.com>
2018-12-18 11:51:23 +0900

    Add elegant CSP support

--- packages/next-playgrounds/src/pages/_document.js
+++ packages/next-playgrounds/src/pages/_document.js
@@ -1,33 +1,45 @@
+import {createHash} from 'crypto';
+import Document, {Head, Main, NextScript} from 'next/document';
 import React from 'react';
 import {ServerStyleSheet} from 'styled-components';
-import Document, {Head, Main, NextScript} from 'next/document';
+
+function createCSPHashOf(text) {
+  const hash = createHash('sha256');
+  hash.update(text);
+  return `'sha256-${hash.digest('base64')}'`;
+}
 
 export default class _Document extends Document {
   static getInitialProps({renderPage}) {
     // Inject `styled-components` styles
     const sheet = new ServerStyleSheet();
     const page = renderPage((Page) => (props) => sheet.collectStyles(<Page {...props} />));
-    const stylesEl = sheet.getStyleElement();
+    const styledEls = sheet.getStyleElement();
 
     return {
       ...page,
-      stylesEl,
+      styledEls,
     };
   }
 
   render() {
-    const {props: {stylesEl}} = this;
+    const shouldEnableHMR = process.env.NODE_ENV === 'development';
+    const nextScriptScriptSource = NextScript.getInlineScriptSource(this.props);
+    const cspRules = [
+      'default-src \'self\'',
+      `script-src 'self' ${shouldEnableHMR ? '\'unsafe-eval\' ' : ''}${createCSPHashOf(nextScriptScriptSource)}`,
+      'style-src \'self\' \'unsafe-inline\'',
+      'img-src \'self\' data:',
+      'font-src \'self\' data:',
+    ];
+
     return (
       <html>
         <Head>
-          {/* CSP rules */}
-          <meta
-            httpEquiv="Content-Security-Policy"
-            content="default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:"
-          />
+          <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1, user-scalable=0, maximum-scale=1, minimum-scale=1" />
+          <meta key="csp" httpEquiv="Content-Security-Policy" content={cspRules.join('; ')} />
 
-          {/* Styles from `styled-components` */}
-          {stylesEl}
+          {this.props.styledEls}
         </Head>
         <body>
           <Main />
