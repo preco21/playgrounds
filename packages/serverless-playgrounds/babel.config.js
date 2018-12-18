227c2dae3963e3dcd8ab87b41fd5980fa7e6d808
Plusb Preco <plusb21@gmail.com>
2018-12-18 12:23:32 +0900

    Apply modern babel support

--- packages/serverless-playgrounds/babel.config.js
+++ packages/serverless-playgrounds/babel.config.js
@@ -1,16 +1,19 @@
-module.exports = () => ({
-  presets: [
-    ['@babel/preset-env', {
-      targets: {
-        node: '8.10',
-      },
-      modules: false,
-      useBuiltIns: 'usage',
-      loose: true,
-    }],
-  ],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
-  ],
-});
+module.exports = (api) => {
+  api.cache.using(() => process.env.NODE_ENV);
+  return {
+    presets: [
+      ['@babel/preset-env', {
+        targets: {
+          node: '8.10',
+        },
+        modules: false,
+        useBuiltIns: 'usage',
+        loose: true,
+      }],
+    ],
+    plugins: [
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-proposal-class-properties',
+    ],
+  };
+};
