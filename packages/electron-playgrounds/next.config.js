bc2453249ad318e4b1da9c498abf44898370c9ae
Plusb Preco <plusb21@gmail.com>
2018-12-18 15:42:04 +0900

    Now with new next config

--- packages/electron-playgrounds/next.config.js
+++ packages/electron-playgrounds/next.config.js
@@ -1,21 +1,61 @@
-const withCSS = require('@zeit/next-css');
 const DotenvPlugin = require('dotenv-webpack');
+const withPlugins = require('next-compose-plugins');
+const withCSS = require('@zeit/next-css');
+const withFonts = require('next-fonts');
 
-module.exports = withCSS({
+function withImagesCustom(nextConfig = {}) {
+  return {
+    ...nextConfig,
+    webpack(config, options) {
+      const {isServer} = options;
+      const {
+        inlineImageLimit = 8192,
+        assetPrefix = '',
+        webpack,
+      } = nextConfig;
+
+      config.module.rules.push({
+        test: /\.(jpe?g|png|gif|ico|webp)$/,
+        loader: 'url-loader',
+        options: {
+          limit: inlineImageLimit,
+          fallback: 'file-loader',
+          publicPath: `${assetPrefix}/_next/static/images/`,
+          outputPath: `${isServer ? '../' : ''}static/images/`,
+          name: '[name]-[hash].[ext]',
+        },
+      });
+
+      config.module.rules.push({
+        test: /\.svg$/,
+        loader: 'svg-url-loader',
+        options: {
+          limit: inlineImageLimit,
+          noquotes: true,
+        },
+      });
+
+      if (typeof webpack === 'function') {
+        return webpack(config, options);
+      }
+
+      return config;
+    },
+  };
+}
+
+module.exports = withPlugins([
+  withCSS,
+  withImagesCustom,
+  withFonts,
+], {
   webpack(config) {
     // HACK: Quick fix to resolve the custom babel config in root directory
     config.module.rules.forEach((rule) => {
-      if (rule.use.loader === 'next-babel-loader') {
+      if (rule.use && rule.use.loader === 'next-babel-loader') {
         // eslint-disable-next-line no-param-reassign
         rule.use.options.cwd = undefined;
       }
-    });
-
-    config.module.rules.push({
-      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-      use: {
-        loader: 'url-loader',
-      },
     });
 
     config.plugins.push(new DotenvPlugin());
