871e3b446e33bbca568233acfa9cc588c53cd36f
Plusb Preco <plusb21@gmail.com>
2019-01-08 12:14:37 +0900

    Update babel config

---
+++ packages/node-playgrounds/babel.config.js
@@ -0,0 +1,19 @@
+module.exports = (api) => {
+  api.cache.using(() => process.env.NODE_ENV);
+  return {
+    presets: [
+      ['@babel/preset-env', {
+        targets: {
+          node: true,
+        },
+        modules: false,
+        useBuiltIns: 'usage',
+        loose: true,
+      }],
+    ],
+    plugins: [
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-proposal-class-properties',
+    ],
+  };
+};
