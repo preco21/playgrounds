1ffde9deff80c75ac08ebe47b307bef8e2fe48e1
Plusb Preco <plusb21@gmail.com>
2019-06-19 10:36:56 +0900

    Update next config for electron template

--- packages/electron-playgrounds/next.config.js
+++ packages/electron-playgrounds/next.config.js
@@ -1,9 +1,26 @@
+const path = require('path')
 const { PHASE_PRODUCTION_BUILD, PHASE_EXPORT } = require('next/constants')
 const DotenvPlugin = require('dotenv-webpack')
 const withPlugins = require('next-compose-plugins')
+const withCustomBabelConfig = require('next-plugin-custom-babel-config')
 const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
 const withFonts = require('next-fonts')
+
+function mixinWebpackConfig(fn) {
+  return ({ webpack, ...nextConfig }, extras) => ({
+    ...nextConfig,
+    webpack(config, options) {
+      fn(config, options, extras)
+
+      if (typeof webpack === 'function') {
+        return webpack(config, options)
+      }
+
+      return config
+    },
+  })
+}
 
 function withElectronProtocolPrefix(nextConfig = {}) {
   return {
@@ -13,52 +30,15 @@ function withElectronProtocolPrefix(nextConfig = {}) {
   }
 }
 
-function withCustomBabelConfig(
-  { webpack, ...nextConfig } = {},
-  { phase } = {},
-) {
-  return {
-    ...nextConfig,
-    webpack(config, options) {
-      const env =
-        phase === PHASE_PRODUCTION_BUILD ? 'production' : 'development'
-
-      config.module.rules.forEach((rule) => {
-        if (rule.use && rule.use.loader === 'next-babel-loader') {
-          rule.use.options.cwd = undefined
-          rule.use.options.envName = `renderer-${env}`
-        }
-      })
-
-      if (typeof webpack === 'function') {
-        return webpack(config, options)
-      }
-
-      return config
-    },
-  }
-}
-
-function withDotenv({ webpack, ...nextConfig } = {}) {
-  return {
-    ...nextConfig,
-    webpack(config, options) {
-      config.plugins.push(new DotenvPlugin())
-
-      if (typeof webpack === 'function') {
-        return webpack(config, options)
-      }
-
-      return config
-    },
-  }
-}
+const withDotenv = mixinWebpackConfig((config) =>
+  config.plugins.push(new DotenvPlugin()),
+)
 
 module.exports = withPlugins([
   withElectronProtocolPrefix,
   withCSS,
   withImages,
   withFonts,
-  withCustomBabelConfig,
+  [withCustomBabelConfig, { babelConfigFile: path.resolve('babel.config.js') }],
   withDotenv,
 ])
